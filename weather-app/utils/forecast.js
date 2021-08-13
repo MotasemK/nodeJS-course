a67040acd43e8535dd71065434e5130bd001240c
MotasemK <mo3tasem66@gmail.com>
2021-08-13 12:27:11 +0300

    Using object short-hand inside the weather-app

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('postman-request')
 const forecast = (latitude, longitude, callback) => {
     
     const url = `http://api.weatherstack.com/current?access_key=d7db7f4b07bb222cc36847ec9840811e&query=${latitude},${longitude}&units=m`
-    request( { url: url, json: true}, (error, response) => {
+    request( { url: url, json: true}, (error, { body }) => {
         if (error){
             callback('Unable to connect to weather service', undefined)
-        } else if (response.body.error){
-            callback(response.body.error.info, undefined)
+        } else if (body.error){
+            callback(body.error.info, undefined)
         } else {
-            const temp = response.body.current.temperature
-            const apperantTemp = response.body.current.feelslike
-            const weatherDescription = response.body.current.weather_descriptions[0]
+            const temp = body.current.temperature
+            const apperantTemp = body.current.feelslike
+            const weatherDescription = body.current.weather_descriptions[0]
             callback(undefined, `${weatherDescription}. It is currently ${temp} degrees out and it feels like ${apperantTemp} degrees out.`)
         }
     })
