77788341a3bf0179e0eecf5c76f2bb52b5fce6e5
MotasemK <mo3tasem66@gmail.com>
2021-08-17 01:06:42 +0300

    Presenting Async-Await

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,6 +1,7 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
+/*
 User.findByIdAndUpdate("611a7232bfa6c352445c1498", { age: 1 })
   .then((user) => {
     console.log(user);
@@ -11,4 +12,19 @@ User.findByIdAndUpdate("611a7232bfa6c352445c1498", { age: 1 })
   })
   .catch((error) => {
     console.log(error);
+  });
+*/
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("611a7232bfa6c352445c1498", 2)
+  .then((count) => {
+    console.log(count);
+  })
+  .catch((e) => {
+    console.log(e);
   });
--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -1,6 +1,7 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/tasks");
 
+/*
 Task.findByIdAndDelete("611ab66a4f74b26b4400ff49")
   .then((task) => {
     console.log(task);
@@ -11,4 +12,19 @@ Task.findByIdAndDelete("611ab66a4f74b26b4400ff49")
   })
   .catch((error) => {
     console.log(error);
+  });
+  */
+// 611a9636fe1479100849589d
+const deleteTaskAndCount = async (id) => {
+  const deletedTask = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount("611a9636fe1479100849589d")
+  .then((count) => {
+    console.log(count);
+  })
+  .catch((e) => {
+    console.log(e);
   });
