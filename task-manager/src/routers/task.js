fd16275aa3c3856ad03c9ea0f3bb24cad12ed393
MotasemK <mo3tasem66@gmail.com>
2021-08-17 13:55:21 +0300

    Finishing lect 11: REST APIs and Mongoose

---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const Task = require("../models/tasks");
+
+/*
+// Create a new router.
+const router = new express.Router();
+// Setup the router.
+router.get("/test", (req, res) => {
+  res.send("This is from my other router");
+});
+// Register it with express application
+app.use(router)
+*/
+
+const router = new express.Router();
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.patch("/tasks/:id", async (req, res) => {
+  const allowedUpdates = ["completed"];
+  const updates = Object.keys(req.body);
+  const isAllowed = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isAllowed) {
+    return res.status(404).send({ error: "Invalid update!" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
