b8d4b55a980627bdd2d5777d9f491b9607743439
MotasemK <mo3tasem66@gmail.com>
2021-08-20 15:59:55 +0300

    Allow clients to send and recive messages

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -14,17 +14,13 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
-  socket.emit("countUpdated", count);
+  socket.emit("message", "Welcome!");
 
-  socket.on("increment", () => {
-    count++;
-
-    io.emit("countUpdated", count); // notify all clients not like socket.emit user client
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
   });
 });
 
