ec1b1bc5822c27442331042062a3f5d8327b27e1
MotasemK <mo3tasem66@gmail.com>
2021-08-20 17:05:20 +0300

    Adding send location feature

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -11,3 +11,17 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
 
   socket.emit("sendMessage", message);
 });
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const location = {
+      lat: position.coords.latitude,
+      long: position.coords.longitude,
+    };
+    socket.emit("sendLocation", location);
+  });
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,14 +2,12 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const { isObject } = require("util");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const port = process.env.port || 3000;
-
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
@@ -18,9 +16,21 @@ io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
   socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
 
   socket.on("sendMessage", (message) => {
     io.emit("message", message);
+  });
+
+  socket.on("sendLocation", (coords) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
   });
 });
 
